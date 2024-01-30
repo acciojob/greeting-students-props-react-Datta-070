d200da820029b879add6ef87ee613b4ab22689e7
Datta-070 <chinmaydatt003@gmail.com>
2024-01-30 13:19:48 +0530

    4.0

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Welcome=({name})=>{
     return(
         <div>
-            <h1>Hey ! {name}</h1>
-            <h2>Welcome to School</h2>
+            <h1>Hey {name}!</h1>
+      <p>Welcome to School.</p>
         </div>
     );
 };
